ab3ecaed5ac38edea69437f42d78be9168fdd9e5
WisterViolet <fujianifan@gmail.com>
2020-07-23 00:37:37 +0900

    Setting Example

---
+++ slackbot_settings_example.py
@@ -0,0 +1,10 @@
+# coding:utf-x
+
+#Bot API Token
+API_TOKEN = 'xxxx-xxxxxxxxxxxx-xxxxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxx'
+
+#Default Reply
+DEFAULT_REPLY = "Default"
+
+#Bot Plugins
+PLUGINS = ['plugins']
